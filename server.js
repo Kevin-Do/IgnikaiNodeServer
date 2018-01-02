ac1ecee86134642f76212d5cf786ce6160f3ff23
Kevin Do <kevindokhoale@gmail.com>
2018-01-02 02:44:28 -0800

    Generate and Emit Client Id

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var defaultPort = 3000;
 var io = require('socket.io')(process.env.PORT || defaultPort);
+var shortid = require('shortid');
 var playerCount = 0;
 
 console.log("Server Started - Port: " + defaultPort);
 
 io.on('connection', function(socket){
-    console.log("\n" + "Client Connected to: " + socket.id);
+    var thisClientId = shortid.generate();
+    console.log("\n" + "Client: " +  thisClientId + "| Connected to: " + socket.id);
 
     //Broadcasts to all connected clients
     socket.broadcast.emit('spawn');
@@ -13,16 +15,19 @@ io.on('connection', function(socket){
 
     for (i = 0; i < playerCount; i++){
         socket.emit('spawn');
-        console.log(socket.id + ' Spawning.');
+        console.log('\nSpawning: ' + thisClientId);
     }
 
     socket.on('move', function(data){
         //Triggers from client Emit()
-        console.log("Client Moved - Data: " + data);
+        data.id = thisClientId;
+        console.log("Client Moved - Data: " + JSON.stringify(data));
+        //Relay "move" to all connected clients (not the orignating client however)
+        socket.broadcast.emit('move', data);
     });
 
     socket.on('disconnect', function(){
+        console.log("\n" + "Client: " + socket.id + " Disconnecting.");
         playerCount--;
-        console.log("/n" + "Client: " + socket.id + " Disconnecting.")
     })
 });
