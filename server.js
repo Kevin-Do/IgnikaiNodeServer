022529f77ecf17b2911773f2b96eeb763fc09ce9
Kevin Do <kevindokhoale@gmail.com>
2018-01-01 17:37:10 -0800

    Spawning Logic
    Disconnections and Player Count

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var defaultPort = 3000;
 var io = require('socket.io')(process.env.PORT || defaultPort);
+var playerCount = 0;
 
-console.log("Server Started: ");
+console.log("Server Started - Port: " + defaultPort);
 
 io.on('connection', function(socket){
-    console.log("Client Connected: " + socket);
+    console.log("\n" + "Client Connected to: " + socket.id);
 
-    socket.on('Move', function(data){
+    //Broadcasts to all connected clients
+    socket.broadcast.emit('spawn');
+    playerCount++;
+
+    for (i = 0; i < playerCount; i++){
+        socket.emit('spawn');
+        console.log(socket.id + ' Spawning.');
+    }
+
+    socket.on('move', function(data){
         //Triggers from client Emit()
-        console.log("Client Moved: " + data);
+        console.log("Client Moved - Data: " + data);
     });
+
+    socket.on('disconnect', function(){
+        playerCount--;
+        console.log("/n" + "Client: " + socket.id + " Disconnecting.")
+    })
 });
